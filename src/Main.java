80771c7d9c456cb82e60ef706b948e221a497b17
rnicholson <rnickle79@gmail.com>
2022-09-23 08:34:25 -0500

    Use lambda instead of anonymous class

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,7 @@ public class Main {
     public static void main(String[] args) {
         new Thread(new BackGroundMusic()).start();
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                new GameFrame(new GamePanel(new Game()));
-            }
-        }).start();
+        new Thread(() -> new GameFrame(new GamePanel(new Game()))).start();
 
     }
 }
